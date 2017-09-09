8437ad4b12f685710efeb073b7bb0255bfea74f7
varun221182 <31116267+varun221182@users.noreply.github.com>
2017-09-09 13:52:42 +1000

    Unit Test, Test3.java
    To test the functionality of calculateAddHocTicketCharge in carpark.java

---
+++ Test3.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcccp;
+
+import java.awt.EventQueue;
+
+import bcccp.carpark.CarSensor;
+import bcccp.carpark.Carpark;
+import bcccp.carpark.Gate;
+import bcccp.carpark.entry.EntryController;
+import bcccp.carpark.entry.EntryUI;
+import bcccp.carpark.exit.ExitController;
+import bcccp.carpark.exit.ExitUI;
+import bcccp.carpark.paystation.PaystationController;
+import bcccp.carpark.paystation.PaystationUI;
+import bcccp.tickets.adhoc.AdhocTicketFactory;
+import bcccp.tickets.adhoc.IAdhocTicket;
+import bcccp.tickets.adhoc.AdhocTicketDAO;
+import bcccp.tickets.adhoc.IAdhocTicketDAO;
+import bcccp.tickets.season.ISeasonTicket;
+import bcccp.tickets.season.ISeasonTicketDAO;
+import bcccp.tickets.season.SeasonTicket;
+import bcccp.tickets.season.SeasonTicketDAO;
+import bcccp.tickets.season.UsageRecordFactory;
+
+
+public class Test3 {
+    
+    public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					
+					IAdhocTicketDAO adhocTicketDAO = new AdhocTicketDAO(new AdhocTicketFactory());
+					ISeasonTicketDAO seasonTicketDAO = new SeasonTicketDAO(new UsageRecordFactory());
+					//Created an instance of carpark
+					Carpark carpark = new Carpark("Bathurst Chase", 5, adhocTicketDAO, seasonTicketDAO);
+					System.out.println("Name");
+                    
+					
+					//Invoking calculateAddHocTicketCharge method for the carpark instance
+                    System.out.println(carpark.calculateAddHocTicketCharge(5));
+                                        
+				} 
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+    
+}
