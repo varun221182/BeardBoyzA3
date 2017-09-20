2e4ae077d6867397ad3186f7fc8a66ec2f520576
Mahendher Reddy Vangala <31062733+ITCSWV1@users.noreply.github.com>
2017-09-20 15:52:00 +1000

    Unit Test - EntryUI
    Testing display()

---
+++ UnitTestingEntryUI2.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcccp;
+
+import java.awt.EventQueue;
+
+import bcccp.carpark.CarSensor;
+import bcccp.carpark.Carpark;
+import bcccp.carpark.Gate;
+import bcccp.carpark.entry.EntryController;
+import bcccp.carpark.entry.EntryUI;
+import bcccp.carpark.exit.ExitController;
+import bcccp.carpark.exit.ExitUI;
+import bcccp.carpark.paystation.PaystationController;
+import bcccp.carpark.paystation.PaystationUI;
+import bcccp.tickets.adhoc.AdhocTicketFactory;
+import bcccp.tickets.adhoc.IAdhocTicket;
+import bcccp.tickets.adhoc.AdhocTicketDAO;
+import bcccp.tickets.adhoc.IAdhocTicketDAO;
+import bcccp.tickets.season.ISeasonTicket;
+import bcccp.tickets.season.ISeasonTicketDAO;
+import bcccp.tickets.season.SeasonTicket;
+import bcccp.tickets.season.SeasonTicketDAO;
+import bcccp.tickets.season.UsageRecordFactory;
+import java.util.Date;
+
+public class UnitTestingEntryUI2 {
+    public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					
+					long entryTime = System.currentTimeMillis();                   
+					//Invoking EntryUI Constructor
+                    EntryUI eui = new EntryUI(320, 100);	
+					eui.display("Hello");
+					                   
+				} 
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
